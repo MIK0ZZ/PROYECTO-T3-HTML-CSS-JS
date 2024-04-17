18e6f3a6bf92a8cce3a101fd439858d8800bb450
MIK0ZZ <beltran.gonzalez@colegiolitterator.com>
2024-04-17 17:19:33 +0200

    Commit 1

---
+++ script.js
@@ -0,0 +1,104 @@
+document.getElementById("b1").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionVocalesAsterisco.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b2").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionSuma.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b3").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionSimaImpares.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b4").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionPalabraMasLargaDeTresLetras.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b5").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionOperacionesVariasConUnArray.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b6").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionNuevoArray.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b7").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionMediaAritmetica.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b8").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionMayorDeDosNumeros.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b9").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionInvertirCadena.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b10").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionEsPar.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b11").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionEsMultiplo.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b12").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionElementosRepetidos.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b13").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionDiaSemana.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b14").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionCuadradoDeLosElementosDeUnArray.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b15").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionCalcularPrecioNeto.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b16").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionCalcularElNumeroMasGrande.html";
+    funciones.style.display = "block";
+});
+
+document.getElementById("b17").addEventListener("click", function() {
+    var funciones = document.getElementById("funciones");
+    funciones.src = "./Funciones/funcionCalculaElSiglo.html";
+    funciones.style.display = "block";
+});
+
+
+
